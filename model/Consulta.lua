2d6bf5018f598da0b3a77238e52d2e306855e1e9
Fernando S. Medeiros <fernando.medeiros.ti@gmail.com>
2017-11-24 17:28:43 -0300

    consertando classe consulta

--- model/Consulta.lua
+++ model/Consulta.lua
@@ -6,21 +6,22 @@ local prioridade = p:criar()
 local status = s:criar()
 local especialidade = e:criar()
 
-local Consulta = {
-	prioridade = nil
-	status = nil
-	data = nil
-	especialidade = nil
-}
+local Consulta = {especialidade, prioridade, status, dataSolicitação, dataAgendamento, paciente}
 
-function Consulta.criar(especialidade, prioridade, status, data)
+function Consulta.criar(especialidade, prioridade, status, dataSolicitação, dataAgendamento, paciente)
 	
 	Consulta.especialidade = especialidade[especialidade]
 	Consulta.status = status[status]
 	Consulta.prioridade = prioridade[prioridade]
-	Consulta.data = data
+	Consulta.dataSolicitação = datadataSolicitação
+	Consulta.dataAgendamento = datadataAgendamento
+	Consulta.paciente = paciente
 
 	return Consulta
+end
+
+function Consulta.json()
+	local json = json.encode(Consulta)
 end
 
 return Consulta
--- model/Endereco.lua
+++ model/Endereco.lua
@@ -1,11 +1,14 @@
 local Endereco = {logradouro, numero, bairro, cep}
 
-function Endereco.criar(logradouro, numero, bairro, cep)
+function Endereco.criar(logradouro, numero, bairro)
 	Endereco.logradouro = logradouro
 	Endereco.numero =  numero
-	Endereco.bairro = bairro 
-	Endereco.cep = cep
+	Endereco.bairro = bairro 	
 	return Endereco
+end
+
+function Endereco.json()
+	local json = json.encode(Paciente)
 end
 
 return Endereco
